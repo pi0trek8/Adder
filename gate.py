feb0b38f4025f3aa0c7ef89065c5d734ff57e840
Pi0trek8 <piotrekszczypior1@gmail.com>
2023-05-21 19:29:23 +0200

    xor gate implementation

--- gate.py
+++ gate.py
@@ -4,3 +4,6 @@ def or_gate(first_signal: int, second_signal: int):
 
 def and_gate(first_signal: int, second_signal: int):
     return 1 if first_signal and second_signal else 0
+
+def xor_gate(first_signal: int, second_signal: int):
+    return first_signal ^ second_signal
