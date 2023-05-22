c1b5c9760ccf21793005fbfe61405a4f67c9ca81
Pi0trek8 <piotrekszczypior1@gmail.com>
2023-05-22 20:32:56 +0200

    command line arguments

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 from prepocessingStage import PreprocessingStage
 from sumComputationStage import SumComputationStage
 import parrarelPrefixStage
+import sys
 
-if __name__ == '__main__':
-    a = [0, 1, 1, 1, 0, 0, 0]
-    b = [0, 1, 1, 1, 0, 1, 1]
-    k = [1, 1, 0, 1, 0, 0, 0]
+
+def main():
+    # a = [0, 1, 1, 1, 0, 0, 0]
+    # b = [0, 1, 1, 1, 0, 1, 1]
+    # k = [1, 1, 0, 1, 0, 0, 0]
+    a = [int(char) for char in sys.argv[1]]
+    b = [int(char) for char in sys.argv[2]]
+    k = [int(char) for char in sys.argv[3]]
 
     preprocessingStage = PreprocessingStage()
     g, p, h, a_prim, b_prim = preprocessingStage.hashed_cells(a, b, k)
-
     g_prim, p_prim, h_prim = preprocessingStage.envelop_cells(a_prim, b_prim)
+    g, p, g_prim, p_prim = parrarelPrefixStage.process(g, p, g_prim, p_prim)
 
     print('p=', p)
     print('g=', g)
 
     print('p`=', p_prim)
     print('g`=', g_prim)
-    # print(a)
-    # print(b)
-    # print(k)
-    # print()
-    # print(g)
-    # print(p)
-    # print(h)
-    # print()
-    # print(g_prim)
-    # print(p_prim)
-    # print(h_prim)
-    g, p, g_prim, p_prim = parrarelPrefixStage.process(g, p, g_prim, p_prim)
-
-    # h = [1, 0, 0, 1, 0, 1, 0]
-    # h_prim = [0, 0, 0, 0, 1, 1, 0]
-    # c = [0, 0, 0, 0, 0, 0, 1]
-    # c_prim = [0, 1, 0, 0, 0, 0, 0]
 
     sumComputationStage = SumComputationStage()
     s = sumComputationStage.compute_s(h, h_prim, g, g_prim)
     s.reverse()
     print()
     print(s)
+
+
+if __name__ == '__main__':
+    main()
